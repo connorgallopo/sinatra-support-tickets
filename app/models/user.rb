4a0869e0ebd4e370e8fc693caafae0b668b7e4d0
Connor Gallopo <connor.gallopo@me.com>
2018-04-10 09:45:59 -0400

    started to build out views

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,3 @@
 class User < ActiveRecord::Base
   has_many :support_tickets
-  has_secure_password
 end
--- db/migrate/20180410133002_create_users_table.rb
+++ db/migrate/20180410133002_create_users_table.rb
@@ -2,7 +2,7 @@ class CreateUsersTable < ActiveRecord::Migration
   def change
     create_table :users do |t|
       t.string :email
-      t.string :password_digest
+      t.string :password
     end
   end
 end
