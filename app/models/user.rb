1277934e6617de2ad04f72182e27c03bb49dbc9b
Connor Gallopo <connor.gallopo@me.com>
2018-04-10 17:47:06 -0400

    added a way to digest encrypted data so the db can be properly seeded

--- app/models/user.rb
+++ app/models/user.rb
@@ -1,4 +1,10 @@
 class User < ActiveRecord::Base
   has_many :support_tickets
   has_secure_password
+
+  def User.digest(string)
+  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                BCrypt::Engine.cost
+  BCrypt::Password.create(string, cost: cost)
+  end
 end
---
+++ db/seeds.rb
@@ -0,0 +1,23 @@
+User.create!(email: 'admin@admin.com',
+             password_digest: User.digest('admin'),
+             role: 'admin')
+User.create!(email: 'needyou@tryingtowork.com',
+             password_digest: User.digest('trucksRcool'),
+             role: 'user')
+User.create!(email: 'struggling@helpme.net',
+             password_digest: User.digest('cats'),
+             role: 'user')
+User.create!(email: 'sally@needssupport.com',
+             password_digest: User.digest('steel'),
+             role: 'user')
+
+SupportTicket.create(subject: 'Cannot log into email!!!!',
+body: 'It says password is invalid, not sure what that means....', user_id: 2)
+SupportTicket.create(subject: 'blank black screen?',
+body: 'My laptop wont turn on... I spilled water on it last week but its been a week so that cant be it...', user_id: 2)
+SupportTicket.create(subject: 'HELP!!!!',
+body: 'My mom sent me this link on facebook... said i won.. is this safe?', user_id: 2)
+SupportTicket.create(subject: 'My dog chewed the cord...',
+body: 'Hi! my dog chewed up my cord... can you send me a new one?', user_id: 3)
+SupportTicket.create(subject: 'Testing the waters...',
+body: 'This is a test of a new ticketing system!', user_id: 3)
