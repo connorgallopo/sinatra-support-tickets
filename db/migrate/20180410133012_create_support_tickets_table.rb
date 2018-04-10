11bdf3641761ad9858e6d10687c3ca7dd128f69e
Connor Gallopo <connor.gallopo@me.com>
2018-04-10 09:33:24 -0400

    built tables in migrations

--- db/migrate/20180410133012_create_support_tickets_table.rb
+++ db/migrate/20180410133012_create_support_tickets_table.rb
@@ -1,4 +1,9 @@
 class CreateSupportTicketsTable < ActiveRecord::Migration
   def change
+    create_table :support_tickets do |t|
+      t.string :subject
+      t.string :content
+      t.integer :user_id
+    end
   end
 end
