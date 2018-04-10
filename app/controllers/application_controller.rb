3f1e34e3b46a6b5415337d7de9c0f34c3f675fe5
Connor Gallopo <connor.gallopo@me.com>
2018-04-10 12:06:43 -0400

    removed pry and setup logout action

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -43,14 +43,17 @@ class ApplicationController < Sinatra::Base
 
   post '/login' do
     @user = User.find_by(email: params['email'])
-    binding.pry
     if @user && @user.authenticate(params['password'])
       session[:user_id] = @user.id
       redirect '/support_tickets'
-      binding.pry
     else
       redirect to '/login'
     end
+  end
+
+  get '/logout' do
+    session.clear if logged_in?
+    redirect to '/login'
   end
 
   def current_user
