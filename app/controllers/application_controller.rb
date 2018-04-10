0c7771d245006c36ea30e2b2d9ea8291747b0cb1
Connor Gallopo <connor.gallopo@me.com>
2018-04-10 12:05:37 -0400

    added create ticket button, added default role of user

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -35,6 +35,8 @@ class ApplicationController < Sinatra::Base
 
   post '/users/new' do
     @user = User.create(params)
+    @user.role = 'user'
+    @user.save
     session[:user_id] = @user.id
     redirect to '/support_tickets'
   end
