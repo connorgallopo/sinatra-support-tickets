c117bde7d400a3ca74e3ec191e7b02e18ac2b7f3
Connor Gallopo <connor.gallopo@me.com>
2018-04-10 10:01:03 -0400

    added login and create user forms

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -1,14 +1,20 @@
 require './config/environment'
 
 class ApplicationController < Sinatra::Base
-
   configure do
     set :public_folder, 'public'
     set :views, 'app/views'
   end
 
-  get "/" do
-    erb :welcome
+  get '/' do
+    erb :index
   end
 
+  get '/login' do
+    erb :'/users/login'
+  end
+
+  get '/users/new' do
+    erb :'/users/new_user'
+  end
 end
