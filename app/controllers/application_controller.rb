dfa0bf0054ac60d3f4d3311f32a69104091d9984
Connor Gallopo <connor.gallopo@me.com>
2018-04-15 22:59:40 -0400

    cleaned up code

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -25,13 +25,13 @@ class ApplicationController < Sinatra::Base
   end
 
   get '/support_tickets' do
-    redirect to '/login' if !logged_in?
+    redirect to '/login' unless logged_in?
     @user = current_user
     erb :'/tickets/tickets'
   end
 
   get '/support_tickets/new' do
-    redirect to '/login' if !logged_in?
+    redirect to '/login' unless logged_in?
     erb :'/tickets/new_ticket'
   end
 
@@ -39,11 +39,11 @@ class ApplicationController < Sinatra::Base
     if logged_in?
       @user = current_user
       @ticket = SupportTicket.find_by(id: params[:id])
-      if @user.role == "admin" || @ticket.user == current_user
+      if @user.role == 'admin' || @ticket.user == current_user
         erb :'/tickets/show'
       end
     else
-      flash[:error] = "ACCESS DENIED"
+      flash[:error] = 'ACCESS DENIED'
       redirect to '/support_tickets'
     end
   end
@@ -52,46 +52,46 @@ class ApplicationController < Sinatra::Base
     if logged_in?
       @user = current_user
       @ticket = SupportTicket.find_by(id: params[:id])
-      if @user.role == "admin" || @ticket.user == current_user
+      if @user.role == 'admin' || @ticket.user == current_user
         erb :'/tickets/edit'
       end
     else
-      flash[:error] = "ACCESS DENIED"
+      flash[:error] = 'ACCESS DENIED'
       redirect to '/support_tickets'
     end
   end
 
   patch '/tickets/:id/edit' do
     @ticket = SupportTicket.find_by(id: params[:id])
-    @ticket.update(subject: params["subject"], body: params["body"])
-    flash[:message] = "Ticket sucessfully edited."
+    @ticket.update(subject: params['subject'], body: params['body'])
+    flash[:message] = 'Ticket sucessfully edited.'
     redirect to '/support_tickets'
   end
 
   post '/users/new' do
-    if valid_email?(params["email"])
+    if valid_email?(params['email'])
       @user = User.create(params)
       @user.role = 'user'
       @user.save
       session[:user_id] = @user.id
       redirect to '/support_tickets'
     else
-      flash[:error] = "Please enter a valid email address."
+      flash[:error] = 'Please enter a valid email address.'
       redirect to '/users/new'
     end
   end
 
   post '/tickets/new' do
-    if (params["subject"] != "" && params["body"] != "")
+    if params['subject'] != '' && params['body'] != ''
       @user = current_user
       @ticket = SupportTicket.create(params)
       @ticket.user = @user
       @ticket.save
-      flash[:message] = "Ticket sucessfully created."
+      flash[:message] = 'Ticket sucessfully created.'
       redirect to '/support_tickets'
     else
-      flash[:error] = "Both Fields must be filled out"
-      redirect to "/support_tickets/new"
+      flash[:error] = 'Both Fields must be filled out'
+      redirect to '/support_tickets/new'
     end
   end
 
@@ -109,20 +109,20 @@ class ApplicationController < Sinatra::Base
     if logged_in?
       @user = current_user
       @ticket = SupportTicket.find_by(id: params[:id])
-      if @user.role == "admin" || @ticket.user == current_user
+      if @user.role == 'admin' || @ticket.user == current_user
         @ticket.delete
-        flash[:message] = "Ticket Deleted."
+        flash[:message] = 'Ticket Deleted.'
         redirect to '/support_tickets'
       end
     else
-      flash[:error] = "You must be logged in to do that."
+      flash[:error] = 'You must be logged in to do that.'
       redirect to '/login'
     end
   end
 
   get '/logout' do
     session.clear if logged_in?
-    flash[:message] = "Logged Out"
+    flash[:message] = 'Logged Out'
     redirect to '/login'
   end
 
--- app/models/user.rb
+++ app/models/user.rb
@@ -2,9 +2,9 @@ class User < ActiveRecord::Base
   has_many :support_tickets
   has_secure_password
 
-  def User.digest(string)
-  cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
-                                                BCrypt::Engine.cost
-  BCrypt::Password.create(string, cost: cost)
+  def self.digest(string)
+    cost = ActiveModel::SecurePassword.min_cost ? BCrypt::Engine::MIN_COST :
+                                                  BCrypt::Engine.cost
+    BCrypt::Password.create(string, cost: cost)
   end
 end
